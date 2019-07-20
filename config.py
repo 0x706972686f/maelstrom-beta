8adf71700fb2e876ad79d264ca5f54919be63c48
0x706972686f <39437196+0x706972686f@users.noreply.github.com>
2019-07-20 15:38:22 +1000

    Add files via upload

--- config.py
+++ config.py
@@ -7,6 +7,10 @@ class Config(object):
     IMAGE_FOLDER = os.path.join(APP_ROOT, 'app/websites')
     TEMP_FOLDER = os.path.join(APP_ROOT, 'app/tmp')
     MAX_CONTENT_LENGTH = 32 * 1024 * 1024
+        
+    # CELERY PARAMETERS
+    CELERY_BROKER_URL = 'redis://localhost:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
     # SQL PARAMETERS
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
